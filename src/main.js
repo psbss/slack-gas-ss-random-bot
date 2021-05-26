d7322fa2512d834d92e633154ff65ff094647790
yuu <uypsbss@gmail.com>
2021-05-26 10:29:54 +0900

    add main.js

---
+++ src/main.js
@@ -0,0 +1,30 @@
+const SPREADSHEET_URL = '// スプレッドシートのURL（非公開でも実行可能）';
+
+function message() {
+  var spreadsheet = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
+  var sheet = spreadsheet.getSheets()[0];
+
+  var user_id_data = sheet.getRange(1, 2, sheet.getLastRow() - 1, 1).getValues();
+  var user_id = [];
+
+  for (var i in user_id_data) {
+    user_id.push(user_id_data[i][0]);
+  }
+  var random = Math.floor(Math.random() * user_id.length);
+
+  var message = `<!channel>\n`
+  message += `本日の議事録担当は<@${user_id[random]}>さんです。よろしくお願いします！`
+  message += `\n<${SPREADSHEET_URL}|候補の編集>`;
+
+  var response = {
+    response_type: "in_channel",
+    text: message,
+  }
+
+  return response;
+}
+
+function doPost(e) {
+  response = message();
+  return ContentService.createTextOutput(JSON.stringify(response)).setMimeType(ContentService.MimeType.JSON)
+}
